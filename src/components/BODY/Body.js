517736c4efef6a3c5ad306a0b1554b14ef18f054
Young - K <appiahkwame274@gmail.com>
2023-08-02 04:11:45 +0000

    added bearer key to env

--- src/components/BODY/Body.js
+++ src/components/BODY/Body.js
@@ -55,7 +55,7 @@ const Body = () => {
         method: "GET",
         headers: {
           accept: "application/json",
-          Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OGY5Y2IyNDM5ODIxMzNkN2Q3NzU3YWU4MTBhMTJlOSIsInN1YiI6IjY0NmZjMzQzNTQzN2Y1MDEyNjNhM2QzMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DiEhFATYQoD8ZLFa_rjLqPKkcq_jmPIbCHTaPF4sX9I",
+          Authorization: process.env.REACT_APP_MOVIE_BEARER,
         },
       });
 
